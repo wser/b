b1cc0ee0d35a3cb61083de9b1084801a5fc8614a
wser <w.web.dev@gmail.com>
2023-10-06 10:25:10 +0200

    some todos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+// import image from "@astrojs/image";
+// import mdx from "@astrojs/mdx";
+// import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    // image({
+    //   serviceEntryPoint: "@astrojs/image/sharp",
+    // }),
+    // mdx(),
+    // sitemap(),
+  ],
 });
